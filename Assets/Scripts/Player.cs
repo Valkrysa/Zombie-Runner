bb0c5615ee67875ad4b5432e9daa28651a4f784b
Alice Robinson <valkrysa@gmail.com>
2017-08-13 23:39:49 -0700

    through 248, nav meshes, spawn points, custom input, custom fov

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public Transform spawnPointsParent;
+    public bool respawnPlayer = false;
+
+    private Transform[] spawnPoints;
+    private bool lastToggle = false;
+
+    public void Start() {
+        spawnPoints = spawnPointsParent.GetComponentsInChildren<Transform>();
+    }
+
+    void Update() {
+        if (lastToggle != respawnPlayer) {
+            respawnPlayer = false;
+            ReSpawn();
+        } else {
+            lastToggle = respawnPlayer;
+        }
+    }
+
+    public void ReSpawn () {
+        int randomSpawnPoint = Random.Range(1, spawnPoints.Length);
+        transform.position = spawnPoints[randomSpawnPoint].transform.position;
+    }
+}
